ee1d7e3440d34c6b078b83e98ffa3eaf1eaee8f2
Jonathan <jonathanmatos.jm71@gmail.com>
2022-10-03 23:35:24 -0300

    Menu concluido

--- lib.py
+++ lib.py
@@ -1,6 +1,3 @@
-from typing import Type
-
-
 def leiaInt(msg):
     while True:
         try:
@@ -29,8 +26,8 @@ def menu(lista):
     cabeçalho('MENU PRINCIPAL')
     c = 1
     for item in lista:
-        print(f'{c} - {item}')
+        print(f'\033[33m{c}\033[m - \033[34m{item}\033[m')
         c += 1
     print(linha())
-    opc = leiaInt('Sua opção: ')
-
+    opc = leiaInt('\033[32mSua opção: \033[m')
+    return opc
--- sistema.py
+++ sistema.py
@@ -1,3 +1,14 @@
-from lib.Interface import *
+from lib import *
+from time import sleep
 
-resposta = menu(['opc1', 'opc2', 'opc3'])
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
+    if resposta == 1:
+        print('Opção 1')
+    elif resposta == 2:
+        print('Opção 2')
+    elif resposta == 3:
+        print('Saindo do sistema... Até logo!')
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
