2436d1aa422cd0be5636e94379b08132f3aa0ab3
Jonathan <jonathanmatos.jm71@gmail.com>
2022-10-09 17:50:03 -0300

    Projeto concluido

--- sistema.py
+++ sistema.py
@@ -1,14 +1,27 @@
 from lib import *
 from time import sleep
 
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
     if resposta == 1:
-        print('Opção 1')
+        # Opção de lista o conteúdo de um arquivo.
+        lerArquivo(arq)
     elif resposta == 2:
-        print('Opção 2')
+        # Opção de cadastrar uma nova pessoa
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
-        print('Saindo do sistema... Até logo!')
+        # Opção de sair do sistema.
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
     else:
         print('\033[31mERRO! Digite uma opção válida!\033[m')
     sleep(2)
